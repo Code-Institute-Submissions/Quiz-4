485b7c03fd6942ef24bdc84f5ee1edc6f754a722
StevieinWaterford <stevieindub@gmail.com>
2021-07-12 14:05:01 +0000

    Add  submission for final project

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,44 +7,49 @@ let resultBox = document.getElementById("result");
 
 myForm.addEventListener('submit', function (event) {
     total = 0;
+    removeHighlightedText();
     event.preventDefault();
     checkAnswers();
 })
 
-
 function checkAnswers() {
 
-    let userAnswers = document.querySelectorAll("input[type=radio]: checked");
+    let userAnswers = document.querySelectorAll("input[type=radio]:checked");
 
     for (var i = 0; i < userAnswers.length; i++) {
         if (userAnswers[i].value == answers[i]) {
-            //push the wrong id into answered Array
-
+            answeredArray.push({
+                id: userAnswers[i].id,
+                answer: 'correct'
+            });
+            total += 1;
+        } else {
             answeredArray.push({
                 id: userAnswers[i].id,
                 answer: 'incorrect'
             });
         }
     }
-
+    displayResult(total, answeredArray);
 }
 
-displayResult(total, answeredArray);
-
-
 function displayResult(total, answeredArray) {
-
     totalDisplay.innerHTML = total;
     resultBox.style.display = "inline";
 
     for (var x = 0; x < answeredArray.length; x++) {
         if (answeredArray[x].answer == 'correct') {
             document.getElementById(answeredArray[x].id).nextElementSibling.classList.add("correct");
-
         } else if (answeredArray[x].answer == 'incorrect') {
-
             document.getElementById(answeredArray[x].id).nextElementSibling.classList.add("incorrect");
         }
     }
+}
 
+function removeHighlightedText() {
+
+    let textList = document.querySelectorAll('label.correct, label.incorrect');
+    for (var y = 0; y < textList.length; y++) {
+        document.getElementById(textList[0].htmlFor).nextElementSibling.classList.remove("incorrect, correct");
+    }
 }
